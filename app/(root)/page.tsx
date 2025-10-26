a1d1ce860c9d2c52ef1d8c98f42e41598dae7ba6
I Gede Yoga Asta Pratama <yoga.asta.pra@gmail.com>
2025-10-26 22:37:33 +0800

    add auth layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Page = () => {
     return (
-        <div className='font-bold text-4xl'>Coming Soon</div>
+        <div className='font-bold text-4xl'>
+            Coming Soon
+        </div>
     )
 }
 export default Page
